d4cd2675144acebf96fd3fc585a5c3148cea9f67
Shisius <neuroslave.rnr@gmail.com>
2021-07-02 19:34:30 +0300

    firmwares folder

--- firmwares/SlaveGanglion/queue_mcu_rnr.h
+++ firmwares/SlaveGanglion/queue_mcu_rnr.h
@@ -2,16 +2,13 @@
 #define _QUEUE_MCU_RNR_H_
 
 #include <atomic>
-#include "Ganglion_Slave_Proto.h"
+#include "../../libc/ganglion_comm/definitions/ganglion_comm_defines.h"
 
-#define MCP_QUEUE_SIZE 256
-// size_bl - size bit length
 class McpQueue
 {
-
 protected:
 
-	McpSample storage[MCP_QUEUE_SIZE];
+	McpSample storage[MCP_QUEUE_DEPTH];
 
 	volatile uint16_t pop_pos;
 	volatile uint16_t push_pos;
@@ -35,7 +32,7 @@ public:
     	d_lock.store(true, std::memory_order_seq_cst);
 		memcpy(storage + push_pos, &sample, sizeof(McpSample));
 		push_pos++;
-    	if (push_pos == MCP_QUEUE_SIZE) push_pos = 0;
+    	if (push_pos == MCP_QUEUE_DEPTH) push_pos = 0;
 		d_lock.store(false, std::memory_order_seq_cst);	
 	}
 
@@ -45,7 +42,7 @@ public:
 		memcpy(sample, storage + pop_pos, sizeof(McpSample));
 		current_index = storage[pop_pos].sample_index;
 		pop_pos++;
-    	if (pop_pos == MCP_QUEUE_SIZE) pop_pos = 0;
+    	if (pop_pos == MCP_QUEUE_DEPTH) pop_pos = 0;
 		d_lock.store(false, std::memory_order_seq_cst);
 	}
 
@@ -56,7 +53,7 @@ public:
 			memcpy(sample, storage + pop_pos, sizeof(McpSample));
 			current_index++;
 			pop_pos++;
-      		if (pop_pos == MCP_QUEUE_SIZE) pop_pos = 0;
+      		if (pop_pos == MCP_QUEUE_DEPTH) pop_pos = 0;
 		} else if (storage[pop_pos].sample_index < current_index + 1) {
 			storage[pop_pos].state = static_cast<uint32_t>(McpSampleState::OLD);
 			memcpy(sample, storage + pop_pos, sizeof(McpSample));
@@ -65,7 +62,7 @@ public:
 			memcpy(sample, storage + pop_pos, sizeof(McpSample));
 			current_index = storage[pop_pos].sample_index;
 			pop_pos++;
-      		if (pop_pos == MCP_QUEUE_SIZE) pop_pos = 0;
+      		if (pop_pos == MCP_QUEUE_DEPTH) pop_pos = 0;
 		}
 		d_lock.store(false, std::memory_order_seq_cst);
 	}
--- libc/ganglion_comm/definitions/ganglion_comm_defines.h
+++ libc/ganglion_comm/definitions/ganglion_comm_defines.h
@@ -8,8 +8,18 @@
 #define GANGLION_CMD_FETCH 0x0
 #define GANGLION_CMD_SAMPLE 0x1
 #define	GANGLION_CMD_START 0x2
+#define GANGLION_CMD_STOP 0x3
 /// Answers
 #define GANGLION_ANS_READY 0x0
 #define GANGLION_ANS_WAIT 0x1
+/// Data defines
+#define GANGLION_N_ELECTRODES 2
+#define MCP_QUEUE_DEPTH 256 
+#define MCP_SAMPLE_BAD 0x7BADBAD7
+/// Data types
+typedef struct {
+	uint32_t sample_index;
+	int32_t eeg_data[GANGLION_N_ELECTRODES];
+} McpSample;
 
 #endif //_GANGLION_COMM_DEFINES_H_
